f0eaf534072042913eb32be16c2a52244ece5006
arnoldas kurbanovas <akurbanovas@albany.edu>
2019-12-06 11:05:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask
 app = Flask(__name__)                                                                                                                                                                       
 @app.route('/')                                                                                                                                                                                     
 def hello_world():                                                                                                                                                                                  
-    return 'Flask working in Docker container!'       
-
-@app.route("/test")
-def salvador():
-    return "Hello, this is a new route"
-
+    return 'Flask working in Docker container!'                                                                                                                 
 if __name__ == "__main__":                                                                                                                                                                          
     app.run(debug=True, host='0.0.0.0')
